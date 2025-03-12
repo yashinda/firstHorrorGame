a78e0cfca9a78a4daa6a64311ea6587625a006bf
yashinda <104185861+yashinda@users.noreply.github.com>
2025-03-13 00:02:54 +0400

    Added a enemy
    Added a enemy, which chasing player if distance between player and enemy less maxDistance

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,8 +3,27 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float maxHealth = 20f;
+    [SerializeField] private Transform Player;
+    [SerializeField] private float moveSpeed = 3f;
+    [SerializeField] private float maxDistanceChase = 25f;
+    [SerializeField] private float minDistanceChase = 10f;
     private bool isDeath = false;
-    
+
+
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, Player.transform.position) <= minDistanceChase)
+        {
+            transform.LookAt(Player.transform);
+            transform.position = Vector3.MoveTowards(transform.position, 
+                Player.transform.position, moveSpeed *  Time.deltaTime);
+        }
+        else if (Vector3.Distance(transform.position, Player.transform.position) >= maxDistanceChase)
+        {
+            transform.rotation = Quaternion.identity;
+        }
+    }
+
     public void TakeDamage(int damageAmount)
     {
         maxHealth -= damageAmount;
@@ -13,6 +32,14 @@ public class Enemy : MonoBehaviour
             isDeath = true;
             Debug.Log("Враг умер");
             Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            Debug.Log("Нанесён урон");
         }
     }
 }
